d00ada572045e6f3531d8790e83df3b75aa494da
Lucas Vega <lucasvega02@gmail.com>
2022-06-19 19:05:54 -0300

    Started exercise 7 and make some changes in previous exercises

---
+++ ejercicio7.py
@@ -0,0 +1,14 @@
+""" 
+Una escuela necesita conocer cuántos alumnos cumplen años en cada mes del año, con el propósito de ofrecerles un agasajo especial en su día. Desarrollar un programa que lea el número de legajo y fecha de nacimiento (día, mes y año) de cada uno de los alumnos que concurren a dicha escuela. La carga finaliza con un número de legajo igual a -1. Emitir un informe donde aparezca (mes por mes) cuántos alumnos cumplen años a lo largo del año. Mostrar también una leyenda que indique cuál es el mes con mayor cantidad de cumpleaños.
+"""
+
+def students(leg, day, month, year):
+
+  birthDay = input('Please enter the day of your birthday: ')
+  birthMonth = input('Please enter the month of your birthday: ')
+  birthYear = input('Please enter the year of your birthday: ')
+
+  monthsCont = [0]*12
+
+students(1134702, 27, 11, 2002)
+

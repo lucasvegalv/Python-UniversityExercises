1d72d08b3023f0ee11766e0afeb7ea0780aa8d8b
Lucas Vega <lucasvega02@gmail.com>
2022-06-20 18:22:46 -0300

    Exercise 7 solved

---
+++ ejercicio8.py
@@ -0,0 +1,1 @@
+"""Escribir una función para devolver la posición que ocupa un valor pasado como parámetro, utilizando búsqueda secuencial en una lista desordenada. La función debe devolver -1 si el elemento no se encuentra en la lista."""
